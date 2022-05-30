1677fcfcc7761a7b0a9e6d2a6a24a4437033e008
caio barroncas <barroncasadc@gmail.com>
2022-05-30 10:43:36 -0400

    ajuste

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// env file
+require __DIR__ . '/../config/environment.php';
+
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
